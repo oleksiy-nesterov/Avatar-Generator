3fafa14355f64b8f71f877f67d07d4dca96ce4cb
Oleksiy Nesterov <oleksiy.nesterov@gmail.com>
2016-04-13 19:30:07 +0300

    update

--- compress.cmd
+++ compress.cmd
@@ -1,5 +1,5 @@
 @echo off
 set APP_HOME=%~dp0www
-java -jar "%DROPBOX%\other\~cmd\yuicompressor.jar" --type css --charset utf-8 "%APP_HOME%\css\styles.css" -o "%APP_HOME%\css\styles-min.css"
-java -jar "%DROPBOX%\other\~cmd\compiler.jar" --js "%APP_HOME%\js\avatar.js" --compilation_level SIMPLE_OPTIMIZATIONS > "%APP_HOME%\js\avatar-min.js"
+java -jar "%DROPBOX%\Webmotoric\~cmd\yuicompressor.jar" --type css --charset utf-8 "%APP_HOME%\css\styles.css" -o "%APP_HOME%\css\styles-min.css"
+java -jar "%DROPBOX%\Webmotoric\~cmd\compiler.jar" --js "%APP_HOME%\js\avatar.js" --compilation_level SIMPLE_OPTIMIZATIONS > "%APP_HOME%\js\avatar-min.js"
 pause
