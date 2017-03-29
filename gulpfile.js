12448acf281d95b74e9ec1638d00f46121a8a75b
Oleksiy Nesterov <oleksiy.nesterov@gmail.com>
2017-03-29 21:18:07 +0300

    small fixes, builder

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,38 @@
 let
 gulp       = require('gulp'),
-eslint     = require('gulp-eslint');
+eslint     = require('gulp-eslint'),
+concat     = require('gulp-concat'),
+uglify     = require('gulp-uglify'),
+sourcemaps = require('gulp-sourcemaps'),
+postcss    = require('gulp-postcss'),
+prefixer   = require('autoprefixer')({browsers:['last 3 version'], remove:false}),
+csswring   = require('csswring')({removeAllComments: true}),
 
-gulp.task('default', function(){
-    gulp.watch(['www/js/*.js']).on('change', function(file){
+js         = ['www/js/avatar.js'],
+css        = ['www/css/avatar.css'];
+
+gulp.task('build.js', function(){
+    return gulp.src(js)
+    .pipe(sourcemaps.init())
+    .pipe(concat('avatar.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('www/js/'));
+});
+
+gulp.task('build.css', function(){
+    return gulp.src(css)
+    .pipe(sourcemaps.init())
+    .pipe(concat('avatar.min.css'))
+    .pipe(postcss([prefixer, csswring]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('www/css/'));
+});
+
+gulp.task('default', ['build.js', 'build.css'], function(){
+    gulp.watch(css, ['build.css']);
+    gulp.watch(js, ['build.js']);
+    gulp.watch(js).on('change', function(file){
         gulp.src(file.path)
         .pipe(eslint({
             rules: {
